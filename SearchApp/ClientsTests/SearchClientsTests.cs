68031fec3ca12b2a2b0764b0d6659bc540a57833
Vladimir Kim <bob08@list.ru>
2020-01-16 22:29:32 +0300

    changed unit tests, added FluentAssertions
    changed unit tests using unit testing best practices, added FluentAssertions for comparing lists of SearchResultModel

--- SearchApp/ClientsTests/SearchClientsTests.cs
+++ SearchApp/ClientsTests/SearchClientsTests.cs
@@ -1,11 +1,11 @@
 using AutoMapper;
 using BingSearchClient.Internal;
+using FluentAssertions;
 using GoogleSearchClient.Internal;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Moq.Protected;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -14,35 +14,206 @@ using Xunit;
 using YandexSearchClient.Internal;
 using Bing = BingSearchClient.Internal.Mapping;
 using Google = GoogleSearchClient.Internal.Mapping;
+using SearchResultModel = Domain.Core.Models.SearchResultModel;
 using Yandex = YandexSearchClient.Internal.Mapping;
 
 namespace ClientsTests
 {
     public class SearchClientsTests
     {
-        private readonly ServiceCollection Services;
-
-        public SearchClientsTests()
+        [Fact]
+        public async Task BingClientTestAsync()
         {
-            var services = new ServiceCollection();
+            var bingClient = SetUpBingClient();
+
+            var results = await bingClient.GetSearchInfoAsync("test");
+
+            var expectedResults = new List<SearchResultModel>()
+            {
+                new SearchResultModel { SearchTerm = "test", Title = "name1", Url = "url1" },
+                new SearchResultModel { SearchTerm = "test", Title = "name2", Url = "url2" },
+                new SearchResultModel { SearchTerm = "test", Title = "name3", Url = "url3" },
+            };
+
+            results.Should().BeEquivalentTo(expectedResults);
+        }
+
+        [Fact]
+        public async Task GoogleClientTestAsync()
+        {
+            var goggleClient = SetUpGoogleClient();
+
+            var results = await goggleClient.GetSearchInfoAsync("test2");
+
+            var expectedResults = new List<SearchResultModel>()
+            {
+                new SearchResultModel { SearchTerm = "test2", Title = "title1", Url = "link1" },
+                new SearchResultModel { SearchTerm = "test2", Title = "title2", Url = "link2" },
+            };
+
+            results.Should().BeEquivalentTo(expectedResults);
+        }
+
+        [Fact]
+        public async Task YandexClientTestAsync()
+        {
+            var yandexClient = SetUpYandexClient();
+
+            var results = await yandexClient.GetSearchInfoAsync("test3");
+
+            var expectedResults = new List<SearchResultModel>()
+            {
+                new SearchResultModel { SearchTerm = "test3", Title = "title1", Url = "url1" },
+                new SearchResultModel { SearchTerm = "test3", Title = "title2", Url = "url2" },
+                new SearchResultModel { SearchTerm = "test3", Title = "title3", Url = "url3" },
+            };
+
+            results.Should().BeEquivalentTo(expectedResults);
+        }
+
+        private BingClient SetUpBingClient()
+        {
+            var services = SetUpBingServiceCollection();
+
+            var httpClient = SetUpBingHttpClient();
+
+            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<BingClient>.Instance;
+            var serviceProvider = services.BuildServiceProvider();
+            var mapper = serviceProvider.GetRequiredService<IMapper>();
+
+            var bingClient = new BingClient(httpClient, serviceProvider.GetRequiredService<BingClientSettings>(), mapper, nullLogger);
+
+            return bingClient;
+        }
+
+        private ServiceCollection SetUpBingServiceCollection()
+        {
             var bingSettings = new BingClientSettings()
             {
                 Url = "http://test.com"
             };
+
+            var services = new ServiceCollection();
+            services.AddSingleton(bingSettings);
+            services.AddSingleton<Profile, Bing.AutoMapperProfile>();
+            services.AddSingleton(
+                sp =>
+                {
+                    var profiles = sp.GetServices<Profile>();
+                    return new MapperConfiguration(
+                            config =>
+                            {
+                                foreach (var profile in profiles)
+                                {
+                                    config.AddProfile(profile);
+                                }
+                            })
+                        .CreateMapper(sp.GetService);
+                });
+
+            return services;
+        }
+
+        private HttpClient SetUpBingHttpClient()
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{\"webPages\":{\"value\":[{\"name\":\"name1\",\"url\":\"url1\"}," +
+                    "{\"name\":\"name2\",\"url\":\"url2\"},{\"name\":\"name3\",\"url\":\"url3\"}]}}")
+                });
+
+            var httpClient = new HttpClient(handlerMock.Object);
+            return httpClient;
+        }
+
+        private GoogleClient SetUpGoogleClient()
+        {
+            var services = SetUpGoogleServiceCollection();
+
+            var httpClient = SetUpGoogleHttpClient();
+
+            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<GoogleClient>.Instance;
+            var serviceProvider = services.BuildServiceProvider();
+            var mapper = serviceProvider.GetRequiredService<IMapper>();
+
+            var bingClient = new GoogleClient(httpClient, serviceProvider.GetRequiredService<GoogleClientSettings>(), mapper, nullLogger);
+
+            return bingClient;
+        }
+
+        private ServiceCollection SetUpGoogleServiceCollection()
+        {
             var googleSettings = new GoogleClientSettings()
             {
                 Url = "http://test.com"
             };
+
+            var services = new ServiceCollection();
+            services.AddSingleton(googleSettings);
+            services.AddSingleton<Profile, Google.AutoMapperProfile>();
+            services.AddSingleton(
+                sp =>
+                {
+                    var profiles = sp.GetServices<Profile>();
+                    return new MapperConfiguration(
+                            config =>
+                            {
+                                foreach (var profile in profiles)
+                                {
+                                    config.AddProfile(profile);
+                                }
+                            })
+                        .CreateMapper(sp.GetService);
+                });
+
+            return services;
+        }
+
+        private HttpClient SetUpGoogleHttpClient()
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+            handlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{\"items\":[{\"title\":\"title1\",\"link\":\"link1\"},{\"title\":\"title2\",\"link\":\"link2\"}]}")
+                });
+
+            var httpClient = new HttpClient(handlerMock.Object);
+            return httpClient;
+        }
+
+        private YandexClient SetUpYandexClient()
+        {
+            var services = SetUpYandexServiceCollection();
+
+            var httpClient = SetUpYandexHttpClient();
+
+            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<YandexClient>.Instance;
+            var serviceProvider = services.BuildServiceProvider();
+            var mapper = serviceProvider.GetRequiredService<IMapper>();
+
+            var yandexClient = new YandexClient(httpClient, serviceProvider.GetRequiredService<YandexClientSettings>(), mapper, nullLogger);
+
+            return yandexClient;
+        }
+
+        private ServiceCollection SetUpYandexServiceCollection()
+        {
             var yandexSettings = new YandexClientSettings()
             {
                 Url = "http://test.com"
             };
 
-            services.AddSingleton(bingSettings);
-            services.AddSingleton(googleSettings);
+            var services = new ServiceCollection();
             services.AddSingleton(yandexSettings);
-            services.AddSingleton<Profile, Bing.AutoMapperProfile>();
-            services.AddSingleton<Profile, Google.AutoMapperProfile>();
             services.AddSingleton<Profile, Yandex.AutoMapperProfile>();
             services.AddSingleton(
                 sp =>
@@ -58,68 +229,11 @@ namespace ClientsTests
                             })
                         .CreateMapper(sp.GetService);
                 });
-            Services = services;
+
+            return services;
         }
 
-        [Fact]
-        public async Task BingClientTestAsync()
-        {
-            var handlerMock = new Mock<HttpMessageHandler>();
-            handlerMock
-                .Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage()
-                {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("{\"webPages\":{\"value\":[{\"name\":\"name1\",\"url\":\"url1\"}," +
-                    "{\"name\":\"name2\",\"url\":\"url2\"},{\"name\":\"name3\",\"url\":\"url3\"}]}}")
-                });
-            var httpClient = new HttpClient(handlerMock.Object);
-
-            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<BingClient>.Instance;
-            var serviceProvider = Services.BuildServiceProvider();
-            var mapper = serviceProvider.GetRequiredService<IMapper>();
-
-            var bingClient = new BingClient(httpClient, serviceProvider.GetRequiredService<BingClientSettings>(), mapper, nullLogger);
-            var results = await bingClient.GetSearchInfoAsync("test");
-            var titles = results.Select(x => x.Title).ToList();
-            var urls = results.Select(x => x.Url).ToList();
-
-            Assert.Equal(3, results.Count);
-            Assert.Equal(new List<string>() { "name1", "name2", "name3" }, titles);
-            Assert.Equal(new List<string>() { "url1", "url2", "url3" }, urls);
-        }
-
-        [Fact]
-        public async Task GoogleClientTestAsync()
-        {
-            var handlerMock = new Mock<HttpMessageHandler>();
-            handlerMock
-                .Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(new HttpResponseMessage()
-                {
-                    StatusCode = HttpStatusCode.OK,
-                    Content = new StringContent("{\"items\":[{\"title\":\"title1\",\"link\":\"link1\"},{\"title\":\"title2\",\"link\":\"link2\"}]}")
-                });
-            var httpClient = new HttpClient(handlerMock.Object);
-
-            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<GoogleClient>.Instance;
-            var serviceProvider = Services.BuildServiceProvider();
-            var mapper = serviceProvider.GetRequiredService<IMapper>();
-
-            var bingClient = new GoogleClient(httpClient, serviceProvider.GetRequiredService<GoogleClientSettings>(), mapper, nullLogger);
-            var results = await bingClient.GetSearchInfoAsync("test");
-            var titles = results.Select(x => x.Title).ToList();
-            var urls = results.Select(x => x.Url).ToList();
-
-            Assert.Equal(2, results.Count);
-            Assert.Equal(new List<string>() { "title1", "title2" }, titles);
-            Assert.Equal(new List<string>() { "link1", "link2" }, urls);
-        }
-
-        [Fact]
-        public async Task YandexClientTestAsync()
+        private HttpClient SetUpYandexHttpClient()
         {
             var handlerMock = new Mock<HttpMessageHandler>();
             handlerMock
@@ -132,20 +246,9 @@ namespace ClientsTests
                     "<hlword>title</hlword>1</title></doc></group><group><doc><url>url2</url><title>title<hlword>2</hlword></title>" +
                     "</doc></group><group><doc><url>url3</url><title>titl<hlword>e</hlword>3</title></doc></group></grouping></results></response></yandexsearch>")
                 });
+
             var httpClient = new HttpClient(handlerMock.Object);
-
-            var nullLogger = Microsoft.Extensions.Logging.Abstractions.NullLogger<YandexClient>.Instance;
-            var serviceProvider = Services.BuildServiceProvider();
-            var mapper = serviceProvider.GetRequiredService<IMapper>();
-
-            var yandexClient = new YandexClient(httpClient, serviceProvider.GetRequiredService<YandexClientSettings>(), mapper, nullLogger);
-            var results = await yandexClient.GetSearchInfoAsync("test");
-            var titles = results.Select(x => x.Title).ToList();
-            var urls = results.Select(x => x.Url).ToList();
-
-            Assert.Equal(3, results.Count);
-            Assert.Equal(new List<string>() { "title1", "title2", "title3" }, titles);
-            Assert.Equal(new List<string>() { "url1", "url2", "url3" }, urls);
+            return httpClient;
         }
     }
 }
